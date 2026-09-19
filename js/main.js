8f6cf4ad24cca7af69bf48b011030184041b73c8
Prathamesh <prathameshdhumal28@gmail.com>
2026-09-20 00:40:43 +0530

    Bump Projects book JS version to invalidate old browser cache

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ ensureStylesheet('css/portfolio-refresh.css');
         './navbar.js?v=20260919a',
         './smooth-scroll.js',
         './animations.js',
-        './repo-carousel.js?v=20260919-photos-v2'
+        './repo-carousel.js?v=20260919-project-book-v1'
       ]
       : isHome
         ? [
