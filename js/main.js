d850a139ecc084f40528f30a34926f52bbbb6381
Prathamesh <prathameshdhumal28@gmail.com>
2026-09-21 00:13:13 +0530

    Invalidate browser cache for updated glass carousel interaction

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ ensureStylesheet('css/portfolio-refresh.css');
         './navbar.js?v=20260919a',
         './smooth-scroll.js',
         './animations.js',
-        './repo-carousel.js?v=20260919-project-book-v1'
+        './repo-carousel.js?v=20260920-glass-reel-v1'
       ]
       : isHome
         ? [
